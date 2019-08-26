317b3b56791bf739ad86a4e2ba85859a4c0ac6b8
Jens Rosseau <jens.rosseau@gmail.com>
2019-08-26 15:05:21 +0200

    Small bug fix

--- Oefening1/src/app/services/overtreding.service.ts
+++ Oefening1/src/app/services/overtreding.service.ts
@@ -13,44 +13,32 @@ export class OvertredingService {
   constructor(private http: HttpClient) { }
 
   getOvertredingen(): Observable<Overtreding[]>{
-    //returned alle pokemon objecten van de meegegeven json
+    
       return this.http.get<Overtreding[]>(this._url);
   }
 
   searchOvertredingStraat(opnameplaats_straat: string, allOvertredingen: Overtreding[]): Overtreding[] {
-    //lege array van pokemons gedeclareerd die moeten worden teruggegeven 
-    const someOvertredingen: Overtreding[] = [];
-    //ingevoerde datums in variabele gestoken
-      //de hele lijst van pokemons wordt geloopt
+   const someOvertredingen: Overtreding[] = [];
       for (const overtreding of allOvertredingen) {
-        //waarbij elke pokemon zijn dataOwned wordt opgeslagen in een variabele
           const straatGezocht = overtreding.opnameplaats_straat
           const aantalGezocht = overtreding.aantal_overtredingen_snelheid;
-          //en vervolgens wordt gechecked of deze pokemon zijn datum tussen de 2 opgegeven datums ligt
+        
           if (opnameplaats_straat === straatGezocht) {
-            //als dit zo is wordt de pokemon toegevoegd aan de array van pokemons die moeten worden teruggegeven
-              someOvertredingen.push(overtreding);
+           someOvertredingen.push(overtreding);
       } 
   }
-    //de array van de gezochte pokemon
-      return someOvertredingen;
+    return someOvertredingen;
     }
 
-    searchOvertredingAantal(aantal_overtredingen_snelheid: number, allOvertredingen: Overtreding[]): Overtreding[] {
-      //lege array van pokemons gedeclareerd die moeten worden teruggegeven 
-      const someOvertredingen: Overtreding[] = [];
-      //ingevoerde datums in variabele gestoken
-        //de hele lijst van pokemons wordt geloopt
+    searchOvertredingAantal(aantal_gezocht: number, allOvertredingen: Overtreding[]): Overtreding[] {
+      const overtredingSnelheid: Overtreding[] = [];
         for (const overtreding of allOvertredingen) {
-          //waarbij elke pokemon zijn dataOwned wordt opgeslagen in een variabele
             const aantalGezocht = overtreding.aantal_overtredingen_snelheid;
-            //en vervolgens wordt gechecked of deze pokemon zijn datum tussen de 2 opgegeven datums ligt
-            if (aantal_overtredingen_snelheid >= aantalGezocht) {
-              //als dit zo is wordt de pokemon toegevoegd aan de array van pokemons die moeten worden teruggegeven
-                someOvertredingen.push(overtreding);
+            if (aantalGezocht >=  aantal_gezocht) {
+                overtredingSnelheid.push(overtreding);
         } 
     }
-      //de array van de gezochte pokemon
-        return someOvertredingen;
+      
+        return overtredingSnelheid;
       }
 }
