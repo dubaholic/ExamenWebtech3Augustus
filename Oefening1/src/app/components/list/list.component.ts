f36921cb34fe08c2aa93be9c9e0207436d6ff182
Jens Rosseau <jens.rosseau@gmail.com>
2019-08-26 15:59:02 +0200

    Hotfix in template

--- Oefening1/src/app/components/list/list.component.ts
+++ Oefening1/src/app/components/list/list.component.ts
@@ -15,6 +15,7 @@ export class ListComponent implements OnInit {
   ngOnInit() {
     this.overtredinsgService.getOvertredingen().subscribe(data => this.overtredings = data)
     this.overtredings.sort();
+    
   }
 
 }
